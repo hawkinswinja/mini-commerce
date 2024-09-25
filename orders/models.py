3e9f8bb21eba64876712b2aca1a337d722fa7cb4
hawkinswinja <hawkinswinja@gmail.com>
2024-09-25 17:04:46 +0300

    update project to monolith backend

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,10 @@
-import uuid
+import random
 from django.db import models
 
 # Create your models here.
-class Order:
-    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    order_date = models.DateTimeField(auto_now_add=True)
-    
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_time = models.DateTimeField(auto_now=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2)
+    order_item = models.CharField(max_length=255)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='orders')
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, viewsets
+from .serializers import OrderSerializer
+from .models import Order
 
-# Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    OrderViewSet API view for CRUD operations on Order objects.
+    This view allows authenticated users to perform CRUD operations on their own orders.
+    It uses the OrderSerializer for serialization and ensures that only authenticated users
+    can access the view.
+    Methods:
+        get_queryset(self):
+            Returns a queryset of orders filtered by the current authenticated user.
+    Attributes:
+        serializer_class (OrderSerializer): The serializer class used for the view.
+        permission_classes (list): A list of permission classes that the user must pass to access the view.
+    """
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user_id=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
