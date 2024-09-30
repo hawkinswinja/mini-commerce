0be741bff8982749e43e8909df154fd3e29e0152
hawkinswinja <hawkinswinja@gmail.com>
2024-09-30 23:44:49 +0300

    feat: notify customers on created orders

---
+++ orders/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Order
+from .sms import send_sms
+
+@receiver(post_save, sender=Order)
+def send_sms_for_order_created(sender, instance, created, **kwargs):
+    print("Order created signal")
+    if created:
+        # recipient = instance.phone
+        recipient = '+254722123123'
+        message = f"Your Order for {instance.product} was successful with order id {instance.order_id}."
+        send_sms.send([recipient], message)
+        
---
+++ orders/sms.py
@@ -0,0 +1,26 @@
+import africastalking
+from minicommerce.settings import AT_USERNAME, AT_APIKEY
+
+
+class SMS:
+    def __init__(self):
+        # get app credentials for authorization
+        self.username = AT_USERNAME  # username in Africastalking app
+        self.api_key = AT_APIKEY  # APIKEY
+
+        # Initialize the SDK
+        africastalking.initialize(self.username, self.api_key)
+
+        # Get the SMS service
+        self.sms = africastalking.SMS
+
+    def send(self, recipients: list, message: str) -> str:
+        try:
+            # send message
+            response = self.sms.send(message, recipients)['SMSMessageData']['Recipients'][0]['status']
+            print(f"Order message sent successful with status: {response}")
+            # return response
+        except Exception as e:
+            print (str(e))
+
+send_sms = SMS()
