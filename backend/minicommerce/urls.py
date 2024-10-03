4819e59a600cc80252ba07b4c54c60b17ced0616
hawkinswinja <hawkinswinja@gmail.com>
2024-10-03 12:58:59 +0300

    fix: update orders url pattern

--- backend/minicommerce/urls.py
+++ backend/minicommerce/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from mozilla_django_oidc.views import OIDCAuthenticationCallbackView, OIDCAuthenticationRequestView
 
 urlpatterns = [
-    # path('oidc/login/', OIDCAuthenticationRequestView.as_view(), name='oidc_login'),
-    # path('oidc/callback/', OIDCAuthenticationCallbackView.as_view(), name='oidc_callback'),
     path('admin/', admin.site.urls),
-    path('api/', include('orders.urls')),
+    path('orders/', include('orders.urls')),
     path('users/', include('users.urls')),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('oidc/', include('mozilla_django_oidc.urls')),
 ]
