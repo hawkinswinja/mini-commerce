06714813d2c2405ce67efa10f6c7c18a9510b376
hawkinswinja <hawkinswinja@gmail.com>
2024-12-10 16:55:13 +0300

    fix: add api prefix to all endpoint routes

--- backend/minicommerce/urls.py
+++ backend/minicommerce/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from mozilla_django_oidc.views import OIDCAuthenticationCallbackView, OIDCAuthenticationRequestView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('orders/', include('orders.urls')),
-    path('user/', include('users.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('oidc/', include('mozilla_django_oidc.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/orders/', include('orders.urls')),
+    path('api/user/', include('users.urls')),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/oidc/', include('mozilla_django_oidc.urls')),
 ]
